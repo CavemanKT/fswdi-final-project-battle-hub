8eb1e82814def27c967dcd8fcdb995cde7df50e4
CavemanKT <vkoubbeago@gmail.com>
2021-12-04 22:59:58 +0800

    a

--- src/components/charts/line-chart.jsx
+++ src/components/charts/line-chart.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react'
 
 import dynamic from 'next/dynamic'
-import useCandidates from '@/_hooks/candidateList'
 
 const Chart = dynamic(() => import('react-apexcharts'), { ssr: false })
 

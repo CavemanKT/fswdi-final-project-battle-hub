7bdfa825713a1a7e57141958b9c98caa7685b519
CavemanKT <vkoubbeago@gmail.com>
2021-12-05 17:29:48 +0800

    doing pagination

--- src/api/controllers/candidates/get.js
+++ src/api/controllers/candidates/get.js
@@ -1,13 +1,21 @@
 import nc from 'next-connect'
 
+import { Op } from 'sequelize'
 import { Profile } from '@/db/models'
 
 const getCandidateList = async (req, res) => {
   const { query: { gameTitle } } = req
 
-  const candidateList = await Profile.findAll({
+  const q = req.query.q || ''
+  const page = Number(req.query.page) || 1
+  const limit = 10
+  const offset = (page - 1) * limit
+  const candidateList = await Profile.findAndCountAll({
     where: {
-      gameTitle
+      gameTitle,
+      characterName: {
+        [Op.iLike]: `%${q}%`
+      }
     },
     include: [
       {
@@ -16,10 +24,17 @@ const getCandidateList = async (req, res) => {
       {
         association: Profile.Histories
       }
-    ]
+    ],
+    order: [['createdAt', 'DESC']],
+    limit,
+    offset
   })
-  console.log(candidateList)
-  res.status(200).json({ candidateList })
+
+  console.log(candidateList.rows)
+
+  res.status(200).json({ candidateList: candidateList.rows,
+    filters: { q, page, limit, offset, totalPages: Math.ceil(candidateList.count / limit) }
+  })
 }
 
 export default nc()
